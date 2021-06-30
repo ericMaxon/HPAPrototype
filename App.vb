4e982e7a3e8b6034d664799bc7bba4ade70ece08
EricMaxon <57864479+ericMaxon@users.noreply.github.com>
2021-06-30 11:02:43 -0500

    Funcionalidad de las pantallas

--- App.vb
+++ App.vb
@@ -1,5 +1,49 @@
 ﻿Public Class App
+
+#Region "Funciones auxiliares del app"
+    Public Sub IrA(Of pantallaANavegar As {Form, New})() ' As {Form, New} agregar esto cuando crees las pantallas
+        Dim pantalla As Form
+        pantalla = pnlMain.Controls.OfType(Of pantallaANavegar).FirstOrDefault() ' Buscar si esta instanciada esta pantalla
+        ' Si no existe la pantalla creamos una nueva
+        If IsNothing(pantalla) Then
+            pantalla = New pantallaANavegar
+            'Estilizando la vista para que se acople a la pantalla principal
+            pantalla.TopLevel = False
+            pantalla.FormBorderStyle = FormBorderStyle.None
+            pantalla.Dock = DockStyle.Fill
+            ' Añadiendola al panel principal
+            pnlMain.Controls.Add(pantalla)
+            pnlMain.Tag = pantalla
+            ' Mostrando la pantalla
+            pantalla.Show()
+            pantalla.BringToFront()
+        Else
+            pantalla.BringToFront()
+        End If
+    End Sub
+#End Region
+
+#Region "App navigation"
+    Private Sub btnHome_Click(sender As Object, e As EventArgs) Handles btnHome.Click
+        IrA(Of HomeForm)()
+    End Sub
+
+    Private Sub btnCitas_Click(sender As Object, e As EventArgs) Handles btnCitas.Click
+        IrA(Of QuotesForm)()
+    End Sub
+
+    Private Sub btnMedicamentos_Click(sender As Object, e As EventArgs) Handles btnMedicamentos.Click
+        IrA(Of MedsForm)()
+    End Sub
+
+    Private Sub btnAccount_Click(sender As Object, e As EventArgs) Handles btnAccount.Click
+        IrA(Of AccountForm)()
+    End Sub
+#End Region
+
+#Region "Funciones propias del 'cel' "
     Private Sub btnCelHome_Click(sender As Object, e As EventArgs) Handles btnCelHome.Click
         Application.Exit()
     End Sub
+#End Region
 End Class
---
+++ Ventanas/MedsForm.vb
@@ -0,0 +1,3 @@
+﻿Public Class MedsForm
+
+End Class
