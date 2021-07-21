19568b1a645d6687c8cbcf9ebedb9813f36a25cf
EricMaxon <57864479+ericMaxon@users.noreply.github.com>
2021-07-21 00:07:05 -0500

    [ADD] Tipo de usuarios

---
+++ Entidades/Paciente.vb
@@ -0,0 +1,10 @@
+﻿Public Class Paciente
+    Private Cedula As String
+    Private Nombre As String
+    Private Telefono As String
+    Private Correo As String
+    Private Edad As Integer
+    Private Altura As Double
+    Private Peso As Double
+
+End Class
