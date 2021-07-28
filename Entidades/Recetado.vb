530092bf0529bc74cf27af86759af8834c5d052b
JOSE PEREZ <jose.perez23@utp.ac.pa>
2021-07-27 19:32:28 -0500

    Clases new

---
+++ Entidades/Recetado.vb
@@ -0,0 +1,41 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Recetado
+
+    Private Codigo As Integer
+    Private cantidad As Integer
+    Private dosis As String
+
+    Public Sub New(_codigo As Integer, _cantidad As Integer, _dosis As String)
+        Codigo = _codigo
+        cantidad = _cantidad
+        dosis = _dosis
+    End Sub
+
+    Public Property CodigoProp As Integer
+        Get
+            Return Codigo
+        End Get
+        Set(value As Integer)
+            Codigo = value
+        End Set
+    End Property
+
+    Public Property CantidadProp As Integer
+        Get
+            Return cantidad
+        End Get
+        Set(value As Integer)
+            cantidad = value
+        End Set
+    End Property
+
+    Public Property DosisProp As String
+        Get
+            Return dosis
+        End Get
+        Set(value As String)
+            dosis = value
+        End Set
+    End Property
+End Class
